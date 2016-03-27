79ef5f76e87c333d3124387b737bfaaa920c0456
adeaver <deaver.andrew@gmail.com>
2016-03-27 14:59:39 -0400

    added check to see if user is allowed to access board

--- passport/accessor.js
+++ passport/accessor.js
@@ -1,16 +1,34 @@
+var Board = require('../boardModel.js');
+
 module.exports.canAccessBoard = function(req, res, next) {
 	// This method should check to see if the board is public or if the user is allowed to access it
+	var boardId = req.params.boardId;
 
 	// GET BOARD AND CHECK IF IT IS PUBLIC HERE
+	Board.findOne({_id:boardId}, function(err, board) {
+		if(!err) {
+			if(board) {
+				if(board.isPublic) {
+					return next;
+				} else {
+					// IF BOARD IS NOT PUBLIC
+					if(req.user != null) {
+						console.log('User is logged in');
 
-	// IF BOARD IS NOT PUBLIC
-	if(req.user != null) {
-		console.log('User is logged in');
+						// check to see if req.user._id is in board ids
+						if(board.users.indexOf(req.user._id) > -1) {
+							return next;
+						} else {
+							res.redirect('/');
+						}
+					}
+			} else {
 
-		// check to see if req.user._id is in board ids
-
-		return next;
-	} else {
-		res.redirect('/');
-	}
+			}
+		} else {
+			res.redirect('/');
+		}
+			}
+		}
+	});
 };
