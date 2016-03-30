681557cae8769d104b8ced70f49d27df24a97047
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-03-30 18:20:58 -0400

    added ellipses too

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -130,9 +130,38 @@ var Canvas = React.createClass({
 		}
 		
 	},
+
+	useEllipse: function() {
+		this.tool.activate();
+		this.tool.onMouseDown = this.onMouseDown;
+		
+		this.tool.onMouseDrag = function(event){
+			var x = event.point.x;
+			var y = event.point.y;
+			// var radius = event.delta.length/2; //the further your mouse movement the bigger the circle
+			var color = {
+						red: 0,
+						green: Math.random(),
+						blue: Math.random(),
+						alpha: ( Math.random() * 0.25 ) + 0.05
+						};
+
+			var ellipse = new Shape.Ellipse({
+				point: [x,y],
+				size: [180,60],
+				fillColor: new Color(color.blue, color.blue, color.green, color.alpha)
+			});
+			// var path = new Path.Rectangle(rectangle);
+		    // path.fillColor = new Color(color.green, color.red, color.blue, color.alpha);
+		    // Refresh the view, so we always get an update, even if the tab is not in focus
+		    view.draw();
+		}
+		
+	},
 	// setTool: function (tool) {
 	// 	tool.activate();
 	// },
+
 
 	render: function () {
 		return (
@@ -142,6 +171,8 @@ var Canvas = React.createClass({
 				<Button setTool={this.useCloud} tool={"Cloud"}/>
 				<Button setTool={this.useCircle} tool={"Circle"}/>
 				<Button setTool={this.useRectangle} tool={"Rectangle"}/>
+				<Button setTool={this.useEllipse} tool={"Ellipse"}/>
+
 				
 			</div>
 		);
