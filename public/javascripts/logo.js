7673665680517381fcaa5d9a2c6000183f0df0af
Jordan Van Duyne <jordannn808@gmail.com>
2016-04-15 14:38:03 -0400

    logout and home buttons

--- public/javascripts/logo.js
+++ public/javascripts/logo.js
@@ -1,9 +1,21 @@
 //module to display name of website
 var Logo = React.createClass({
 	render: function () {
+		var logout = (<p></p>);
+		var home = (<span></span>);
+
+		if (this.props.isLoggedIn) {
+			logout = <a className='mainNav' href='/logout'>Logout</a>;
+		}
+
+		if (this.props.homeButton) {
+			home = (<a className='mainNav' href='/'>Home</a>);
+		}
 		return (
 			<div className='header'>
-				<span className='pavement'> Pavement</span>
+				<span className='pavement'>Pavement</span>
+				{logout}
+				{home}
 			</div>
 		);
 	}
