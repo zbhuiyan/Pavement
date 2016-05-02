eb6d177de4e3bc49e8abfc11b36774fba8eb4fa7
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2016-05-01 21:34:42 -0400

    cleaning up a lil

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-	username:String,
-	password:String
+	username: String,
+	password: String
 });
 
 module.exports = mongoose.model('User', userSchema);
